fd9d0f6c5bfc5c7ce2407111f2ad6fc8d4f55e09
br1an17 <brianzairpaez@gmail.com>
2022-09-13 09:36:08 -0300

    primera tarea

---
+++ homework/homework.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+
+    let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
+}
+
+console.log(BinarioADecimal("1011"));
+
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  var array = [];
+  while (num > 0) {
+    array.unshift(num % 2);
+    num = Math.trunc(num / 2)
+  }
+    return array.join('');
+  }
+
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
