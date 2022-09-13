9dde6346364d7e4a76a9bac3827e50f4b8c22fd2
br1an17 <brianzairpaez@gmail.com>
2022-09-13 09:45:24 -0300

    ahora si

--- homework/homework.js
+++ homework/homework.js
@@ -6,12 +6,12 @@ function BinarioADecimal(num) {
     let sum = 0;
 
     for (let i = 0; i < num.length; i++) {
-       sum += +num[i] * 2 ** (num.length - 1 - i);
+       sum = sum + num[i] * 2 ** (num.length - 1 - i);
     }
     return sum;
 }
 
-console.log(BinarioADecimal("1011"));
+ console.log(BinarioADecimal("1011"));
 
 
 function DecimalABinario(num) {
